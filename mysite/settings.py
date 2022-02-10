9d8b14a5d6945c050fd1dea56150cf949494e400
mayo <rayiszafar@gmail.com>
2022-02-10 23:49:07 +0500

    live

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b&i(9hr!qbds^2_z8n)=7mmu!d3q^yxu%-x(2wo683y+(lf+h("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["faraon90.pythonanywhere.com", "www.faraon90.pythonanywhere.com"]
 
 
 # Application definition
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
